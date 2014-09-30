33c5e202eb602c4c17c74c2de2f3d7ff64916d07
Nessus <nessus85100@hotmail.com>
2014-10-01 01:19:54 +0300

    Update to v3.1.6 (see changelog for details).

--- extras/scripts/setLanguage.py
+++
@@ -1,6 +0,0 @@
-import sys
-from xbmcjson import XBMC
-
-languageToSet = sys.argv[1]
-xbmc = XBMC("http://localhost:80/jsonrpc")
-xbmc.Settings.SetSettingValue({"setting":"locale.language","value":languageToSet})
